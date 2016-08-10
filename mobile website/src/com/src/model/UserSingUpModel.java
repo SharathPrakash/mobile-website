27282952bb1a247507d4aed85432d04d322fb74b
raghvendra <ragavendra@gmail.com>
2016-08-10 12:51:48 +0530

    asdf

--- mobile website/src/com/src/model/UserSingUpModel.java
+++ mobile website/src/com/src/model/UserSingUpModel.java
@@ -2,26 +2,17 @@ package com.src.model;
 
 public class UserSingUpModel {
 
-	private String firstName;
-	private String city;
-	private String lastName;
-	private String emailId;
-	private String password;
-	private String repassword;
-	private String address;
-	private String state;
-	private String pincode;
-	private String mobnum;
+	private String firstName,lastName,emailId,password,address,city,state,pincode,mobnum;
 
-	public UserSingUpModel(String firstName,String lastName, String city,String emailId, String password, String repassword
-			,String address,String state,String pincode, String mobnum)
+	public UserSingUpModel(String firstName,String lastName,String emailId,String password,String address,String city,String state,String pincode,String mobnum)
 	{
 		this.firstName = firstName;
 		this.city = city;
 		this.lastName = lastName;
 		this.emailId = emailId;
 		this.password = password;
-		this.repassword = repassword;
+		
+		
 		this.address = address;
 		this.state = state;
 		this.pincode = pincode;
@@ -30,9 +21,19 @@ public class UserSingUpModel {
 		
 		
 	}
-	
-	public String getFirst_name() {
+
+	public String getFirstName() {
 		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	public String getPassword() {
+		return password;
 	}
 	public String getAddress() {
 		return address;
@@ -40,22 +41,6 @@ public class UserSingUpModel {
 
 	public String getCity() {
 		return city;
-	}
-
-	public String getLast_name() {
-		return lastName;
-	}
-
-	public String getEmail_ID() {
-		return emailId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getRepassword() {
-		return repassword;
 	}
 
 	public String getState() {
